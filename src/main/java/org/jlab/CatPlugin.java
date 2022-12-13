c190b03d429dc161d2fb94ae28536460592d4bea
slominskir <ryan.slominski@gmail.com>
2022-12-13 11:44:59 -0500

    Initial commit

---
+++ src/main/java/org/jlab/CatPlugin.java
@@ -0,0 +1,11 @@
+package org.jlab;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class CatPlugin implements Plugin<Project> {
+    @Override
+    public void apply(Project project) {
+
+    }
+}
---
+++ src/main/java/org/jlab/CatTask.java
@@ -0,0 +1,39 @@
+package org.jlab;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.file.FileCollection;
+import org.gradle.api.file.RegularFileProperty;
+import org.gradle.api.tasks.InputFiles;
+import org.gradle.api.tasks.OutputFile;
+import org.gradle.api.tasks.TaskAction;
+
+import java.io.*;
+import java.util.Iterator;
+
+public abstract class CatTask extends DefaultTask {
+
+    @InputFiles
+    public abstract FileCollection getInput();
+
+    @OutputFile
+    public abstract RegularFileProperty getOutput();
+
+    @TaskAction
+    public void run() throws IOException {
+        File outFile = getOutput().getAsFile().get();
+
+        try(PrintWriter writer = new PrintWriter(outFile)) {
+            for (Iterator<File> it = getInput().iterator(); it.hasNext(); ) {
+                File f = it.next();
+                try(BufferedReader br = new BufferedReader(new FileReader(f))) {
+
+                    String line = br.readLine();
+                    while (line != null) {
+                        writer.println(line);
+                        line = br.readLine();
+                    }
+                }
+            }
+        }
+    }
+}
