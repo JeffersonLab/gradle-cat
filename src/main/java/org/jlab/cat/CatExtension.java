eb05560f3f8967e799525bd28277b830bfc3cbf7
slominskir <ryan.slominski@gmail.com>
2023-01-04 09:31:32 -0500

    javadoc comments

--- src/main/java/org/jlab/cat/CatExtension.java
+++ src/main/java/org/jlab/cat/CatExtension.java
@@ -6,10 +6,23 @@ import org.gradle.api.provider.ListProperty;
 import org.gradle.api.tasks.InputFiles;
 import org.gradle.api.tasks.OutputFile;
 
+/**
+ * Gradle Extension for the concatenation plugin.
+ */
 public abstract class CatExtension {
+    /**
+     * Input files (ordered lazy list).
+     *
+     * @return The input files
+     */
     @InputFiles
     public abstract ListProperty<RegularFile> getInput();
 
+    /**
+     * Output file (lazy non-directory/non-device File).
+     *
+     * @return The output file
+     */
     @OutputFile
     public abstract RegularFileProperty getOutput();
 }
